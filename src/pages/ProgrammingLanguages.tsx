c5ed68556af7614b9c8b7cefe94a8643e426f722
Ruhi Pirnia <ruhiyyih@Ruhis-MacBook-Air.local>
2024-09-08 17:43:08 -1000

    programming languages page

--- src/pages/ProgrammingLanguages.tsx
+++ src/pages/ProgrammingLanguages.tsx
@@ -1,21 +1,79 @@
 import * as React from "react"; 
 import { createRoot } from "react-dom/client";
+import LoadingImage from "../components/LoadingImage";
 import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
+
 
 const ProgrammingLanguages = () => { 
+    const [languageData, setLanguageData] = useState([]); 
+
+    const [pageReady, setPageReady] = useState(false); 
+
+    const [canMap, setCanMap] = useState(false);
+
+    useEffect(() => { 
+
+        const fetchData = async () => { 
+        
+        try { 
+        
+        const username = "apiUser"; 
+        
+        const password = "apiPassword"; 
+        
+        const encodedCredentials = btoa(`${username}:${password}`); 
+        
+        const headers = new Headers(); headers.append("Authorization", `Basic ${encodedCredentials}`); 
+        
+         
+        
+        const res = await fetch( `https://8bit-backend-mock-project.vercel.app/api/programming-languages`, { method: "GET", headers: headers }); 
+        
+         
+        
+        if (res.ok) { 
+        
+        const data = await res.json();
+        
+        console.log(data); 
+        
+        setLanguageData(data); 
+        
+        setCanMap(true); 
+        
+        } 
+        
+        } catch (e: any) { 
+        
+        console.log(e); 
+        
+        } finally { 
+        
+        setPageReady(true); 
+        
+        } 
+        
+        }; 
+        
+         
+        
+        fetchData(); 
+        
+        }, []);
 
 return ( 
 
-<> 
-<Navbar/>
+    <> 
+    <Navbar/>
 
-<div className="container text-left my-5" style={{paddingTop:100}}>
+        <div className="container text-left my-5" style={{paddingTop:100}}>
 
-This is the programming language page 
+            This is the programming language page 
 
-</div> 
+        </div> 
 
-</> 
+    </> 
 
 ); 
 
